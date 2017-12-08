51f8d4ea4dbcdc7105e9fd28e0b4d6e04cee0b63
FERNANDO ALBUQUERQUE <fer.albuquerque90@gmail.com>
2017-12-08 21:59:12 -0200

    Estrutura de pastas MVVM e Pages criadas

--- UWP.AvaliacaoFinal/MainPage.xaml.cs
+++ UWP.AvaliacaoFinal/MainPage.xaml.cs
@@ -1,17 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.UI.Xaml;
+﻿using UWP.AvaliacaoFinal.ViewModel;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -22,9 +10,12 @@ namespace UWP.AvaliacaoFinal
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        public MainPageViewModel ViewModel { get; } = new MainPageViewModel();
+
         public MainPage()
         {
             this.InitializeComponent();
         }
+
     }
 }
---
+++ UWP.AvaliacaoFinal/ViewModel/MainPageViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UWP.AvaliacaoFinal.Pages;
+using UWP.AvaliacaoFinal.Services;
+using Windows.UI.Xaml;
+
+namespace UWP.AvaliacaoFinal.ViewModel
+{
+    public class MainPageViewModel
+    {
+
+        public void AddReceita_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate<IncluirReceitaPage>();
+        }
+    }
+}
