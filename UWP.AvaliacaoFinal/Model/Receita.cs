abb7d9f8ded7946a98fe8562dff7e6fed725eda5
FERNANDO ALBUQUERQUE <fer.albuquerque90@gmail.com>
2017-12-14 21:49:37 -0200

    Criação Model

---
+++ UWP.AvaliacaoFinal/Model/Receita.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UWP.AvaliacaoFinal.Model
+{
+    public class Receita
+    {
+        public int Id { get; set; }
+        public string Titulo { get; set; }
+        public string ModoPreparo { get; set; }
+        public string Ingredientes { get; set; }
+        public TipoReceita TipoReceita { get; set; }
+    }
+
+    public class TipoReceita
+    {
+        public int Id { get; set; }
+        public string Descricao { get; set; }
+    }
+}
